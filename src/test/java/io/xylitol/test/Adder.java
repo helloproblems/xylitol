9ee561f20dddd99bed9e4f9bcaa40e4c16690ce2
xuyandong <ydxu@91jkys.com>
2018-01-22 17:41:21 +0800

    commit update readme.md

--- src/test/java/io/xylitol/test/Adder.java
+++ src/test/java/io/xylitol/test/Adder.java
@@ -13,13 +13,13 @@ import io.xylitol.util.concurrent.Promise;
  */
 public class Adder {
     public static void main(String[] args) throws Exception {
-        Promise<Integer> futureTest_1 = new AdditionTestFuture().addListener(future -> System.out.println("hello I'm the first " + future.get()));
-        Promise<Integer> futureTest_2 = new AdditionTestFuture().addListener(future -> System.out.println("hello I'm the second " + future.get()));
+        Promise<Integer> futureTest_1 = new AdditionTestFuture().addListener(future -> System.out.println("hello I'm the add " + future.get()));
+        Promise<Integer> futureTest_2 = new AdditionTestFuture().addListener(future -> System.out.println("hello I'm the multiply " + future.get()));
 
 
         TaskQueueHandler taskQueueHandler = new TaskQueueHandler();
-        TaskContext taskContext = new DefaultPromiseTaskContext<>(Adder.class, "add", new Object[]{1, 2}, futureTest_1);
-        TaskContext _taskContext = new DefaultPromiseTaskContext<>(Adder.class, "add", new Object[]{5, 2}, futureTest_2);
+        TaskContext taskContext = new DefaultPromiseTaskContext<>(Adder.class, "add", new Object[]{3, 7}, futureTest_1);
+        TaskContext _taskContext = new DefaultPromiseTaskContext<>(Adder.class, "multiply", new Object[]{5, 12}, futureTest_2);
         taskQueueHandler.taskAdded(taskContext);
         taskQueueHandler.taskAdded(_taskContext);
         taskQueueHandler.start();
@@ -28,5 +28,10 @@ public class Adder {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
 
 }
